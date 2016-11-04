5f7632546793fdf92e94f7d7961af1f8609852c7
Aaron Watson <aaron.watson@ahold.com>
2016-11-04 08:42:31 -0400

    Check if file is a symlink before copying it into the "old" folder

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -6,8 +6,8 @@
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
+dir=$HOME/dotfiles                    # dotfiles directory
+olddir=$HOME/dotfiles_old             # old dotfiles backup directory
 files="bash_profile vim vimrc"    # list of files/folders to symlink in homedir
 
 ##########
@@ -24,9 +24,13 @@ echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    if [[ -L "$HOME/.$file" ]]; then
+        echo "$HOME/.$file is already a symlink"
+    else
+        echo "Moving $file from ~ to $olddir"
+        mv $HOME/.$file $HOME/dotfiles_old/
+        echo "Creating symlink to $file in home directory."
+        ln -s $dir/$file $HOME/.$file
+   fi
 done
 
