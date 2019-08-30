1ff282efa590c8d8009676ce51f9362ebdd4cf1c
Aaron Watson <aaron@a7902vlan401dhcp7.us.royalahold.net>
2019-08-30 09:10:37 -0400

    clean up

--- set_proxy.sh
+++ set_proxy.sh
@@ -5,8 +5,12 @@ echo -e ""
 read -s -p "Enter Password: " mypass
 echo -e ""
 
-export HTTP_PROXY=http://$myid:$mypass@proxy.aholdusa.com:8080
-export HTTPS_PROXY=http://$myid:$mypass@proxy.aholdusa.com:8080
-export SOCKS_PROXY=http://$myid:$mypass@proxy.aholdusa.com:8080
+mypass=${mypass//@/%40}
+mypass=${mypass//:/%3A}
+mypass=${mypass//!/%21}
+mypass=${mypass//#/%23}
+mypass=${mypass//$/%24}
+
+export {http,https,socks,ftp,all}_proxy=http://$myid:$mypass@proxy.aholdusa.com:8080
 
 echo -e "Proxies set!\n"
